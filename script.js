ad3c04d50e987ee266cde52b52228806e1517d3e
pxxsx35 <teerapuwanai1@gmail.com>
2025-11-12 08:02:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-500px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+    fetch("navbar.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+      });
